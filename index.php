2fac76d833e5985a2a521b8b93b15a1bcb07a5cb
WASZKA009 <81929351+WASZKA009@users.noreply.github.com>
2023-01-09 16:14:01 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>CMS WEBSITE</title>
+        <link rel="stylesheet" href="./Source/HomePageContent/PageCss/style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <!-- Banner -->
+            <div class="row">
+                <div class="col-12 banner">
+                    <h1 class="title">STRONA GŁÓWNA</h1>
+                </div>
+            </div>
+            <!-- Body -->
+            <div class="row pb-5">
+                <div class="col-7 offset-1">
+                    <div class="content">
+                        <div class="page">
+                            <h1 class="pageTitle">
+                                &#60;TITLE&#62;
+                            </h1>
+                            <h3 class="pageSubtitle">
+                                &#60;SUBTITLE&#62;
+                            </h3>
+                            <h5 class="smth">
+                                &#60;SMTH&#62;
+                            </h5>
+                        </div>
+                        <img class="add-button" src="./Source/HomePageContent/PagePhotos/add-icon.png" alt="Add_Button"/>
+
+                    </div>
+                </div>
+                <div class="col-2 offset-1">
+                    <div class="menu">
+                        <h5>MENU</h5>
+                    </div>
+                </div>
+            </div>
+            <!-- Footer -->
+            <div class="row footer">
+                <p class="col-4">
+                    Copyright &copy;2022
+                </p>
+                <p class="col-4">
+                    Oskar Waszkiewicz
+                </p>
+                <p class="col-4">
+                    IVGTP
+                </p>
+            </div>
+        </div>    
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    </body>
+</html>
